5183364b3f2485b56e7335895e809fc82ad4c130
M0NDI <armond_93@live.co.uk>
2023-08-06 20:57:51 +0100

    added more nav elements and improved styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,40 +7,56 @@ import companyLogo from "../assets/images/companyLogo.svg";
 
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <nav className="navbar mb-8 fixed bg-zinc-100 w-full h-20">
-        <ul className="flex justify-between items-center h-full font-extrabold text-xs">
-          <div className="mr-20 min-w-12 ml-4">
-            <img src={companyLogo} alt="company logo" />
-          </div>
-          <li className="ml-2 hover:underline">
+    <div className="navbar-container h-full w-full flex flex-wrap">
+      <nav className="navbar mb-8 fixed w-full h-20 outline flex justify-between items-center">
+        <div className="company-logo ml-6">
+          <a href="/">
+            <img src={companyLogo} alt="company logo" className="h-10 min-h-10"/>
+          </a>
+        </div>
+        <ul className="flex justify-end items-center font-extrabold text-xs h-full">
+          <li className="p-5 hover:underline h-full w-15 hover:cursor-pointer flex items-center">
             <a href="/">SHOP</a>
           </li>
-          <li className="mr-2 hover:underline">
+          <li className="p-5 hover:underline h-full w-15 hover:cursor-pointer flex items-center">
             <a href="/">BARISTA SCHOOLS</a>
           </li>
-          <li className="mr-2 hover:underline">
+          <li className="p-5 hover:underline h-full w-15 hover:cursor-pointer flex items-center">
+            <a href="/collections/coffee-subscriptions">SUBSCRIPTIONS</a>
+          </li>
+          <li className="p-5 hover:underline h-full w-15 hover:cursor-pointer flex items-center">
             <a href="/">WHOLESALE</a>
           </li>
-          <li className="mr-2 hover:underline">
+          <li className="p-5 hover:underline h-full w-15 hover:cursor-pointer flex items-center">
             <a href="/">LOCATIONS</a>
           </li>
-          <li className="mr-2 hover:underline">
+          <li className="p-5 hover:underline h-full w-15 hover:cursor-pointer flex items-center">
             <a href="/">OUR STORY</a>
           </li>
-
-          <div className="account-icons flex w-1/5 justify-between items-center ml-8">
-            <div className="w-8">
-              <img src={searchIcon} alt="Search Icon" />
-            </div>
-            <div className="w-8">
-              <img src={briefcaseIcon} alt="Briefcase Icon" />
-            </div>
-            <div className="w-8">
-              <img src={profileIcon} alt="BProfile Icon" />
-            </div>
-          </div>
         </ul>
+        <div className="account-icons flex items-center mr-6">
+          <div className="w-8">
+            <img
+              src={searchIcon}
+              alt="Search Icon"
+              className="h-5 cursor-pointer"
+            />
+          </div>
+          <div className="w-8">
+            <img
+              src={profileIcon}
+              alt="Profile Icon"
+              className="h-4 cursor-pointer"
+            />
+          </div>
+          <div className="w-8">
+            <img
+              src={briefcaseIcon}
+              alt="Briefcase Icon"
+              className="h-4 cursor-pointer"
+            />
+          </div>
+        </div>
       </nav>
     </div>
   );
