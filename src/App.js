785975e96004d3a7a84d9cb1c8cda6407b22e84a
M0NDI <armond_93@live.co.uk>
2023-09-01 20:40:18 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
 import ProductPage from "./components/ProductPage";
